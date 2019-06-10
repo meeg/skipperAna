9c9e3ec8c7ed7b7718e7f92eed8d8a5c273e0c0f
Sho Uemura <meeg@slac.stanford.edu>
2019-06-10 14:06:29 -0500

    spurious_charge.py now plots all HDUs

--- spurious_charge.py
+++ spurious_charge.py
@@ -19,7 +19,7 @@ NROWS = 700 #number of rows read out, per file
 
 OHDU = 1
 
-gain = 400
+gain = 300
 
 OHDPLOTS = False
 OSMPLOTS = False
@@ -57,7 +57,9 @@ print(EXPOSURE,READOUT)
 #if (len(sys.argv)<3):
 #    print("not enough input args")
 #    sys.exit(1)
-outfilename = remainder[0]
+outfilename = remainder[0].rpartition(".")[0] #strip off file extension, if any
+if outfilename=="":
+    outfilename = remainder[0]
 
 data = TChain("skPixTree")
 osm = TChain("osMeanTree")
@@ -100,6 +102,20 @@ c.Print(outfilename+".pdf[")
 
 c.SetLogy(1)
 
+
+
+ohdus = []
+ohduelists = []
+fitvals = []
+badFitList = []
+
+for ohdu in range(1,5):
+    elistname = "e{0}".format(ohdu)
+    n = data.Draw(">>"+elistname,"ohdu=={0}".format(ohdu))
+    if n>0:
+        ohdus.append(ohdu)
+        ohduelists.append(gDirectory.Get(elistname))
+
 if (OSMPLOTS):
     if isMonsoon:
         osm.Draw("osm","spl>20")
@@ -128,22 +144,36 @@ if (OHDPLOTS):
 
 ohducut = "ohdu=={0}".format(OHDU)
 
-data.Draw("pix>>hdata"+pixbinning,"x>8 && y>0 && "+ohducut,"colz")
-hdata = gDirectory.Get("hdata")
+hists = []
+c.Clear()
+c.Divide(len(ohdus),1)
+for iHdu in range(0,len(ohdus)):
+    c.cd(iHdu+1)
+    gPad.SetLogy(1)
 
-#c.Print(outfilename+".pdf");
-#c.Print(outfilename+".pdf]");
-#sys.exit(0)
+    hname = "hdata{0}".format(ohdus[iHdu])
 
-fitvals={}
-badFit = skipper_utils.fitPeaksGaus(hdata,gain,fitvals)
+    data.SetEventList(ohduelists[iHdu])
+    data.Draw("pix>>"+hname+pixbinning,"x>8 && y>0","colz")
+    hists.append(gDirectory.Get(hname))
+
+    fitvals.append({})
+    badFitList.append(skipper_utils.fitPeaksGaus(hists[iHdu],gain,fitvals[iHdu]))
+
+c.cd()
 c.Print(outfilename+".pdf");
 
 print(fitvals)
 
-if not badFit:
-    skipper_utils.fitPeaksPoisson(fitfunc,hdata,fitvals)
-    c.Print(outfilename+".pdf");
+for iHdu in range(0,len(ohdus)):
+    c.cd(iHdu+1)
+    gPad.SetLogy(1)
+    if not badFitList[iHdu]:
+        s = skipper_utils.fitPeaksPoisson(fitfunc,hists[iHdu],fitvals[iHdu])
+        badFitList[iHdu] = (int(s)!=0)
+
+c.cd()
+c.Print(outfilename+".pdf");
 
 #data.Draw("pix>>hdata"+pixbinning,"x>0 && y>0 && "+ohducut,"colz")
 #hdata = gDirectory.Get("hdata")
@@ -164,85 +194,101 @@ exposures = [0.0, (362.0/450)*READOUT/NROWS, READOUT*(624.0/NROWS), 0.5*READOUT*
 latex = TLatex()
 latex.SetNDC(True)
 c.Clear()
-c.Divide(1,4)
-
-histList = []
-for hnum in range(0,4):
-    c.cd(hnum+1)
-    gPad.SetLogy(1)
-    hname = "h"+htypes[hnum]
-    data.Draw("pix>>"+hname+pixbinning,regioncuts[hnum]+" && "+ohducut,"colz")
-    h = gDirectory.Get(hname)
-    histList.append(h)
-    if not badFit:
-        s = skipper_utils.fitPeaksPoisson(fitfunc,h,fitvals)
-        if (int(s)==0): #good fit
-            arrPoisson.append(s.Parameter(4))
-            arrPoissonErr.append(s.Error(4))
-            arrActiveTime.append(exposures[hnum])
-            arrZero.append(0.0)
-            latex.DrawLatex(0.3,0.8,"mu={0} \pm {1}".format(s.Parameter(4),s.Error(4)))
+c.Divide(len(ohdus),4)
+histList = [] #not used, but needed so Python doesn't delete the histograms
+for iHdu in range(0,len(ohdus)):
+    data.SetEventList(ohduelists[iHdu])
+    for hnum in range(0,4):
+        c.cd(iHdu + hnum*len(ohdus) +1)
+        gPad.SetLogy(1)
+        hname = "h{0}{1}".format(ohdus[iHdu],htypes[hnum])
+        data.Draw("pix>>"+hname+pixbinning,regioncuts[hnum],"colz")
+        h = gDirectory.Get(hname)
+        histList.append(h)
+        if not badFitList[iHdu]:
+            s = skipper_utils.fitPeaksPoisson(fitfunc,h,fitvals[iHdu])
+            if (int(s)==0): #good fit
+                #arrPoisson.append(s.Parameter(4))
+                #arrPoissonErr.append(s.Error(4))
+                #arrActiveTime.append(exposures[hnum])
+                #arrZero.append(0.0)
+                latex.DrawLatex(0.3,0.8,"mu={0} \pm {1}".format(s.Parameter(4),s.Error(4)))
 
 
 c.cd()
 c.Print(outfilename+".pdf");
 c.Clear()
 
-if not badFit:
-    print(arrPoisson)
-    print(arrPoissonErr)
-    print(arrActiveTime)
-    c.SetLogy(0)
-    gStyle.SetStatX(0.4)
-    thegraph = TGraphErrors(len(arrPoisson),arrActiveTime,arrPoisson,arrZero,arrPoissonErr)
-    thegraph.Draw("AP")
-    thegraph.Fit("pol1")
-    thegraph.SetMinimum(0.0)
-    thegraph.SetMarkerSize(10)
-    c.Print(outfilename+".pdf");
+#if not badFit:
+    #print(arrPoisson)
+    #print(arrPoissonErr)
+    #print(arrActiveTime)
+    #c.SetLogy(0)
+    #gStyle.SetStatX(0.4)
+    #thegraph = TGraphErrors(len(arrPoisson),arrActiveTime,arrPoisson,arrZero,arrPoissonErr)
+    #thegraph.Draw("AP")
+    #thegraph.Fit("pol1")
+    #thegraph.SetMinimum(0.0)
+    #thegraph.SetMarkerSize(10)
+    #c.Print(outfilename+".pdf");
+#c.SetLogy(1)
+#gStyle.SetStatX(0.9)
+
+pixcuts = [("any","ele>0.6"),
+        ("small","ele>0.6 && ele<3.5"),
+        ("big","ele>10"),
+        ("neg","ele<-0.7")]
+
+gStyle.SetOptStat(0)
+c.Clear()
+c.Divide(len(ohdus),4)
+histList = [] #not used, but needed so Python doesn't delete the histograms
+for iHdu in range(0,len(ohdus)):
+    data.SetEventList(ohduelists[iHdu])
+    if not badFitList[iHdu]:
+        data.SetAlias("ele","(pix-{0})/{1}".format(fitvals[iHdu]["zero"],fitvals[iHdu]["gain"]))
+
+        #gPad.SetLogy(1)
+        #data.Draw("ele>>hdata(500,-1,4)","x>8 && y>0 && "+ohducut,"colz")
+        #c.Print(outfilename+".pdf");
+
+        for hnum in range(0,4):
+            c.cd(iHdu + hnum*len(ohdus) +1)
+            hname = "h2d{0}_{1}".format(ohdus[iHdu],pixcuts[hnum][0])
+            binning = "(450,-0.5,449.5,700,-0.5,699.5)"
+            print data.Draw("y:x>>"+hname+binning,"x>0 && y>0 && "+pixcuts[hnum][1],"")
+c.cd()
+c.Print(outfilename+".pdf");
+
+
 c.SetLogy(1)
-gStyle.SetStatX(0.9)
+gStyle.SetOptStat(0)
 
-
-if not badFit:
-    data.SetAlias("ele","(pix-{0})/{1}".format(fitvals["zero"],fitvals["gain"]))
-
-    data.Draw("ele>>hdata(500,-1,4)","x>8 && y>0 && "+ohducut,"colz")
-    c.Print(outfilename+".pdf");
-
-    c.SetLogy(0)
-    gStyle.SetOptStat(0)
-    c.Clear()
-    c.Divide(2,2)
-    c.cd(1)
-    data.Draw("y:x>>h2d_any(450,-0.5,449.5,700,-0.5,699.5)","x>0 && y>0 && ele>0.6 && "+ohducut,"colz")
-
-    c.cd(2)
-    data.Draw("y:x>>h2d_small(450,-0.5,449.5,700,-0.5,699.5)","x>0 && y>0 && ele>0.6 && ele<3.5 && "+ohducut,"colz")
-
-    c.cd(3)
-    data.Draw("y:x>>h2d_big(450,-0.5,449.5,700,-0.5,699.5)","x>0 && y>0 && ele>3.5 && "+ohducut,"colz")
-
-    c.cd(4)
-    data.Draw("y:x>>h2d_neg(450,-0.5,449.5,700,-0.5,699.5)","x>0 && y>0 && ele<-0.7 && "+ohducut,"colz")
-
-    c.cd()
-    c.Print(outfilename+".pdf");
-    c.Clear()
-    c.SetLogy(1)
-    gStyle.SetOptStat(110011)
+linfunc = TF1("linfunc","[0]*(x+[1])",1,624)
+linfunc.FixParameter(1,(638.0*READOUT/EXPOSURE - 1))
+#p0+p1*1 = x*READOUT
+#p0+p1*639 = x*(EXPOSURE+READOUT)
+#p1*638 = x*EXPOSURE
+#p1 = x*EXPOSURE/638
+#p0 = x*(READOUT - EXPOSURE/638)
+#p0/p1 = 638*READOUT/EXPOSURE - 1
+hists = []
+c.Clear()
+c.Divide(len(ohdus),3)
+for iHdu in range(0,len(ohdus)):
+    data.SetEventList(ohduelists[iHdu])
     #data.Draw("ele>>hdata(75,-2.5,22.5)","x>0 && y>0 && "+ohducut,"colz")
     #c.Print(outfilename+".pdf");
 
-    data.Draw(">>elist","x>0 && y>0 && ele>0.6 && ele<3.5 && "+ohducut)
+    data.Draw(">>elist","x>0 && y>0 && ele>0.6 && ele<3.5")
     data.SetEventList(gDirectory.Get("elist"))
 
-    data.Draw("ele>>hdata(500,0,4)","","colz")
-    c.Print(outfilename+".pdf");
-    c.SetLogy(0)
+    #data.Draw("ele>>hdata(500,0,4)","","colz")
+    #c.Print(outfilename+".pdf");
+    #c.SetLogy(0)
 
-    data.Draw("x>>hdata(450,-0.5,449.5)","","colz")
-    c.Print(outfilename+".pdf");
+    c.cd(iHdu + 0*len(ohdus) +1)
+    data.Draw("x>>h{0}x(450,-0.5,449.5)".format(ohdus[iHdu]),"","colz")
 
     #data.Draw("x>>hdata(45,-0.5,449.5)","","colz")
     #c.Print(outfilename+".pdf");
@@ -250,27 +296,20 @@ if not badFit:
     #data.Draw("x>>hdata(200,359.5,379.5)","x>0 && y>0 && pix>250","colz")
     #c.Print(outfilename+".pdf");
 
-    data.Draw("y>>hdata(700,-0.5,699.5)","x>=8 && x<=369","colz")
-    c.Print(outfilename+".pdf");
+    c.cd(iHdu + 1*len(ohdus) +1)
+    data.Draw("y>>h{0}y(700,-0.5,699.5)".format(ohdus[iHdu]),"x>=8 && x<=369","colz")
 
-    data.Draw("y>>hdata(26,0.5,624.5)","x>=8 && x<=369","colz")
-    h = gDirectory.Get("hdata")
+    c.cd(iHdu + 2*len(ohdus) +1)
+    data.Draw("y>>h{0}y2(26,0.5,624.5)".format(ohdus[iHdu]),"x>=8 && x<=369","colz")
+    h = gDirectory.Get("h{0}y2".format(ohdus[iHdu]))
 
-    linfunc = TF1("linfunc","[0]*(x+[1])",1,624)
-    linfunc.FixParameter(1,(638.0*READOUT/EXPOSURE - 1))
-    #linfunc = TF1("linfunc","pol1",1,624)
-    #linfunc.SetParameters(h.Integral()/26/(1 + ((1+639)*0.5)/(638*READOUT/EXPOSURE - 1)),h.Integral()/26/(638*READOUT/EXPOSURE - 1 + (1+639)*0.5))
     h.Sumw2()
-    h.Fit("linfunc")
-    #p0+p1*1 = x*READOUT
-    #p0+p1*639 = x*(EXPOSURE+READOUT)
-    #p1*638 = x*EXPOSURE
-    #p1 = x*EXPOSURE/638
-    #p0 = x*(READOUT - EXPOSURE/638)
-    #p0/p1 = 638*READOUT/EXPOSURE - 1
+    h.Fit("linfunc","Q")
     gStyle.SetStatX(0.4)
 
-    c.Print(outfilename+".pdf");
+c.cd()
+c.Print(outfilename+".pdf");
+
 
     #data.Draw("y>>hdata(200,600,650)","x>=8 && x<=369","colz")
     #c.Print(outfilename+".pdf");
@@ -282,3 +321,5 @@ if not badFit:
     #c.Print(outfilename+".pdf");
 
 c.Print(outfilename+".pdf]");
+#c.Print(outfilename+".pdf]");
+#sys.exit(0)
