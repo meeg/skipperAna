1b01c8f2bc7608754a1465dec9f7cef2b7e859b0
Sho Uemura <meeg@slac.stanford.edu>
2019-07-03 13:11:16 -0500

    update gitignore

--- run_plots_scan.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./spurious_charge_scan.py -e 120 plots_30MAY2019_scan_12 30MAY2019_DarkCurrent_clkscan/skp_10min_vddOFF_*_12.root
-./spurious_charge_scan.py -e 120 plots_30MAY2019_scan_13 30MAY2019_DarkCurrent_clkscan/skp_10min_vddOFF_*_13.root
-./spurious_charge_scan.py -e 120 plots_30MAY2019_scan_14 30MAY2019_DarkCurrent_clkscan/skp_10min_vddOFF_*_14.root
-./spurious_charge_scan.py -e 120 plots_30MAY2019_scan_15 30MAY2019_DarkCurrent_clkscan/skp_10min_vddOFF_*_15.root
